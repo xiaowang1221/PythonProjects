864db15b22d904d684fa5280b4467b0255824e7d
Xiao Wang <hmw@g.com>
2024-10-29 19:36:01 +0900

    d2完成主业务逻辑

--- d2.2048/main.py
+++ d2.2048/main.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Matrix2048():
+class Matrix2048:
     """实现业务逻辑"""
 
     def __init__(self, column: int = 4):
@@ -16,8 +16,6 @@ class Matrix2048():
         """
         生成新数字
         """
-        matrix = self.matrix
-        column = self.column
         # 找出所有 0 的位置
         zero_positions = [(i, j) for i in range(len(self.matrix))
                           for j in range(len(self.matrix[i])) if self.matrix[i][j] == 0]
@@ -37,12 +35,12 @@ class Matrix2048():
         # 水平方向有相同数字则未结束
         for row in range(self.column):
             for col in range(self.column - 1):  # 注意数组不要越界
-                if self.matrix[row][col] == self.matrix[row][col+1]:
+                if self.matrix[row][col] == self.matrix[row][col + 1]:
                     return False
         # 垂直方向有相同数字则未结束
         for row in range(self.column - 1):
             for col in range(self.column):
-                if self.matrix[row][col] == self.matrix[row+1][col]:
+                if self.matrix[row][col] == self.matrix[row + 1][col]:
                     return False
         # 以上条件不满足则游戏结束
         return True
@@ -55,19 +53,173 @@ class Matrix2048():
         self.generate_num()
         self.generate_num()
 
+    @staticmethod
+    def compress(row):
+        """
+        将行里非0数字左移，并在右侧添加0
+        :param row: list
+        :return: list
+        """
+        new_row = [num for num in row if num != 0]  # 移除0
+        return new_row + [0] * (len(row) - len(new_row))  # 补齐0
+
+    @staticmethod
+    def merge(row):
+        """
+        合并数字，向左移动
+        :param row:list
+        :return: list
+        """
+        for i in range(len(row) - 1):
+            if row[i] == row[i + 1] and row[i] != 0:
+                row[i] *= 2  # 相同数字合并
+                row[i + 1] = 0
+        return row
+
     def matrix_move(self):
         """
+        实现矩阵的左移合并
+        """
+        for i in range(self.column):
+            self.matrix[i] = self.compress(self.matrix[i])  # 左移
+            self.matrix[i] = self.merge(self.matrix[i])  # 合并
+            self.matrix[i] = self.compress(self.matrix[i])  # 再次左移，清理合并后产生的空位
 
+    def reverse(self):
+        """
+        反转矩阵中的每一行，用于向右移动
+        """
+        self.matrix = [row[::-1] for row in self.matrix]
+
+    def transpose(self):
+        """
+        转置矩阵，用于实现上下移动
+        """
+        self.matrix = [list(row) for row in zip(*self.matrix)]
+
+    def move(self, position: str):
+        """
+        根据移动方向对矩阵进行移动和合并
+        :param position: 移动方向 ('left', 'right', 'up', 'down')
+        """
+        if position == 'left':
+            self.matrix_move()
+        elif position == 'right':
+            self.reverse()  # 反转矩阵
+            self.matrix_move()
+            self.reverse()  # 恢复矩阵
+        elif position == 'up':
+            self.transpose()  # 转置矩阵
+            self.matrix_move()
+            self.transpose()  # 恢复矩阵
+        elif position == 'down':
+            self.transpose()
+            self.reverse()
+            self.matrix_move()
+            self.reverse()
+            self.transpose()
+
+        # 每次移动后生成一个新的数字
+        self.generate_num()
+
+
+class Window2048:
+    """
+    建立窗体GUI
+    """
+    def __init__(self, column: int = 4):
+        self.init_setting(column)
+        self.data = Matrix2048(column)
+        self.root = self.init_root()
+        self.t = 0  # 判断游戏结束时用
+        self.main()
+
+
+    def init_setting(self, column):
+        """
+        配置初始化
+        :param column:
+        :return:
+        """
+        self.column = column
+        # 棋盘中格子的间隔，单位为px
+        self.space_size = 12
+
+        # 棋盘中格子的大小，单位为px
+        self.cell_size = 80
+
+        # 用于存储tkinter.Lable对象
+        self.emts = []  # 存储lable对象
+
+        # 用于存储游戏的样式信息，如背景色，字体颜色，字体大小等
+        self.style = {
+            'page': {'bg': '#d6dee0', },
+            # 0 ~ 4 灰色系  bg 背景颜色， fg字体颜色， fz字体大小
+            0: {'bg': '#EEEEEE', 'fg': '#EEEEEE', 'fz': 30},
+            2 ** 1: {'bg': '#E5E5E5', 'fg': '#707070', 'fz': 30},
+            2 ** 2: {'bg': '#D4D4D4', 'fg': '#707070', 'fz': 30},
+            # 8 ～ 16 橙色系
+            2 ** 3: {'bg': '#FFCC80', 'fg': '#FAFAFA', 'fz': 30},
+            2 ** 4: {'bg': '#FFB74D', 'fg': '#FAFAFA', 'fz': 30},
+            # 32 ～ 64 红色系
+            2 ** 5: {'bg': '#FF7043', 'fg': '#FAFAFA', 'fz': 30},
+            2 ** 6: {'bg': '#FF5722', 'fg': '#FAFAFA', 'fz': 30},
+            # 128～2048 黄色系
+            2 ** 7: {'bg': '#FFEE58', 'fg': '#FAFAFA', 'fz': 30},
+            2 ** 8: {'bg': '#FFEB3B', 'fg': '#FAFAFA', 'fz': 30},
+            2 ** 9: {'bg': '#FDD835', 'fg': '#FAFAFA', 'fz': 30},
+            # 1024~2048 橙色系
+            2 ** 10: {'bg': '#FF9800', 'fg': '#FAFAFA', 'fz': 30},
+            2 ** 11: {'bg': '#FB8C00', 'fg': '#FAFAFA', 'fz': 28},
+            # 4096 +  红色系
+            2 ** 12: {'bg': '#fb3030', 'fg': '#FAFAFA', 'fz': 28},
+            2 ** 13: {'bg': '#e92e2e', 'fg': '#FAFAFA', 'fz': 28},
+            2 ** 14: {'bg': '#da1e1e', 'fg': '#FAFAFA', 'fz': 24},
+            # 2**15 +  黑色 超过2**15颜色不再改变
+            2 ** 15: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 22},
+            2 ** 16: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 20},
+            2 ** 17: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 20},
+            2 ** 18: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 20},
+            2 ** 19: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 18},
+            2 ** 20: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 17},
+            2 ** 21: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 16},
+            2 ** 22: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 15},
+            2 ** 23: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 14},
+            2 ** 24: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 13},
+            2 ** 25: {'bg': '#3a3a3a', 'fg': '#E0E0E0', 'fz': 12},
+        }
+
+    def init_root(self):
+        """
+        窗口初始化
+        :return:
+        """
+
+    def update_ui(self):
+        """
+        更新UI
         :return:
         """
         pass
 
-    def move(self, position):
+    def key_event(self, event):
         """
-        向移动方位移动
-        :param position: 移动方向
+        事件循环
+        :param event:键盘事件
         :return:
         """
         pass
 
+    def reset_game(self):
+        """
+        重置游戏
+        :return:
+        """
+        pass
+
+    def main(self):
+        """
+        主程序
+        :return:
+        """
 
